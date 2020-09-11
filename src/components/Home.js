31e8b23c0496ecf560c196b11790a366b01f0321
Osman Warsi <osman.warsi@live.com>
2020-09-11 19:45:44 -0400

    Added secondary colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,28 +15,14 @@ function Home() {
             <Container id="dark" fluid>
             <Row>
                 <Col> 
-            <div className="title">GENERATE COLOR SCHEMES <span id="pink"> ON </span><span id="green">THE </span><span id="bluee">GO</span> </div>
+            <div className="title">GENERATE COLOR SCHEMES <br/>
+             <span id="pink"> ON </span><span id="green">THE </span><span id="bluee">GO</span> </div>
             
                
                 
                 </Col>
-                <Col>
-                <Media>
-                    <img
-                        width={100}
-                        height={100}
-                        className="mr-3"
-                        src={wheel}
-                        alt="Generic placeholder"
-                    />
-                    <Media.Body id="">
-                        <h4 id="pink2">Color schemes for your branding</h4>
-                        <h5 id="whitee">
-                       Using color theory, multiple color palletes, UI, HTML/CSS code are generated.
-                        </h5>
-                    </Media.Body>
-                    </Media>
-                </Col>
+              
+            
             </Row>
             <Row>
             <div className="title">
